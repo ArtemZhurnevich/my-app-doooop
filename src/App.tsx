c9e65f2d36ee0b649828a762e783f7621c8da751
Artem <zhur2305@gmail.com>
2022-11-25 19:13:52 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Todolist from './Todolist';
+
+
+type PropsType = {
+    body: string
+    id: number
+    title: string
+    userId: number
+
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = useState<PropsType[]>([])
+
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setState(json))
+    }, [])
+
+    const deleteHandler = () => {
+        setState([])
+    }
+    const showpostsHandler = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setState(json))
+
+    }
+    return (
+        <div className="App">
+            <button onClick={deleteHandler}>DELETE</button>
+            <button onClick={showpostsHandler}>showposts</button>
+            <ul>
+                {state.map((el) => {
+
+                    return (<li key={el.id}>
+                            <li><span>{el.id}</span></li>
+                            <li><span>{el.userId}</span></li>
+                            <li><span>{el.title}</span></li>
+                            <li><span>{el.id}</span></li>
+
+                        </li>
+                    )
+                })}
+            </ul>
+
+
+
+        </div>
+    );
 }
 
 export default App;
